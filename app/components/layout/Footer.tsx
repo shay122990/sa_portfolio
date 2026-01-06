fe49d52a039afe41fa80db26b85e1879fb58f6d8
shay asanova <moeshakasanova@gmail.com>
2026-01-06 13:53:06 +0400

    root layout change, navbar and footer to full width

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ import { Magnetic } from "../ui/Magnetic";
 
 export function Footer() {
   return (
-    <footer className="relative z-10 mx-auto max-w-7xl px-5 py-5">
+    <footer className="relative z-10 mx-auto max-w-7xl px-5 py-5 w-full">
       <div className="flex flex-col items-center justify-between gap-4 rounded-2xl border border-white/10 bg-white/5 p-5 text-center backdrop-blur-xl md:flex-row md:text-left">
         <div>
           <b className="text-white">Let’s build something users love.</b>
--- app/layout.tsx
+++ app/layout.tsx
@@ -26,11 +26,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full overflow-x-hidden">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen overflow-x-hidden`}
-      >
+      <body className="min-h-screen flex flex-col">
         <Navbar />
-        <main className="w-full overflow-x-hidden">{children}</main>
+        <main className="w-full flex-1 overflow-x-hidden">
+          <div className="mx-auto max-w-7xl px-5">{children}</div>
+        </main>
+
         <Footer />
       </body>
     </html>
